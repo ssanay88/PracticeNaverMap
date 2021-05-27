4b1552202606baa83974f28250824b889b80ca3e
Yangsuhyuck <pplloo748@gmail.com>
2021-05-27 20:30:51 +0900

    해야할 것 05.27 마커 원하는 곳으로 지정 - 완료 현재 위치 불러와서 추적할 수 있도록 하기 -> 현재 위치 값 불러오기 - 완료 reverse geocoding을 통해 좌표값 주소로 전환하여 읽기 - 미완성

--- app/src/main/java/com/example/practicenavermap/MainActivity.kt
+++ app/src/main/java/com/example/practicenavermap/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.practicenavermap
 
 import android.content.Context
+import android.location.Geocoder
 import android.location.LocationManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Transformations.map
@@ -16,14 +18,20 @@ import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
+import org.naver.Naver
 import java.util.jar.Manifest
 
 
 class MainActivity : AppCompatActivity(),OnMapReadyCallback {
 
+    val TAG = "로그"
+
     private lateinit var locationSource: FusedLocationSource
     private lateinit var naverMap: NaverMap
     private lateinit var mainBinding: ActivityMainBinding
+    private var geocoder : Geocoder = Geocoder(this)
+    // 네이버 지오코더 때문에 선언
+    val naver = Naver(clientId = "9yulgtl3nq", clientSecret = "R9A3oPxTIi1CTqnETRNNGZ5VSFRoSjHPY00biFax")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,8 +76,8 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback {
     override fun onMapReady(naverMap: NaverMap) {
         // 마커 객체 생성 및 지도에 추가
         val marker = Marker()
-        marker.position = LatLng(35.15788038125036, 129.05922326871536)
-        marker.map = naverMap
+//        marker.position = LatLng(35.15788038125036, 129.05922326871536)
+
 
         // 위치 오버레이 객체 - 자신의 위치를 표현할 때
         val locationOverlay = naverMap.locationOverlay
@@ -78,12 +86,36 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback {
 
         // naverMap에 locarionSource를 지정하면 위치추적 기능을 사용할 수 있게된다.
         naverMap.locationSource = locationSource
+
         // 위치 추적 모드에 대해 정할 수 있다
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
-        // 위치 변경에 대한 이벤트를 정할 수 있다.
+
+        // 위치 변경에 대한 이벤트를 정할 수 있다. 위치 변경 시 감지지
         naverMap.addOnLocationChangeListener { location ->
             mainBinding.textView.text = location.latitude.toString() + location.longitude.toString()
         }
+
+        // 지도가 클릭되면 onMapClick() 콜백 메서드가 호출되며, 파라미터로 클릭된 지점의 화면 좌표와 지도 좌표가 전달됩니다.
+        naverMap.setOnMapClickListener { pointF, coord ->
+            marker.position = LatLng( coord.latitude , coord.longitude )
+            marker.map = naverMap
+
+            Log.d(TAG, "마커 생성 ${coord.latitude} ${coord.longitude} ")
+
+            var location =  geocoder.getFromLocation(coord.latitude,coord.longitude,1)
+
+
+            // mainBinding.textView.text = coord.latitude.toString() + coord.longitude.toString()
+//            val locations = naver.map().reverseGeocode(lat = coord.latitude.toString(), lng = coord.longitude.toString())
+            Log.d(TAG, "우당탕탕ㅇ ${location[0]}")
+            // locations.items.forEach { location -> location.address }
+
+
+            //mainBinding.textView.text = locations.toString()
+        }
+
+
+
 
     }
 
